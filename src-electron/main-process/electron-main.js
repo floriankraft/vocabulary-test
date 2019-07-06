664b5a5d60bcaf6506e3ba04fc02919e1a82d1b5
Florian Kraft <mail@floriankraft.com>
2019-07-06 18:30:02 +0200

    Add first proof, that we can send file content from main- to renderer-process.

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,4 +1,6 @@
 import { app, BrowserWindow } from 'electron'
+import fs from 'fs'
+import path from 'path'
 
 /**
  * Set `__statics` path to static files in production;
@@ -10,36 +12,75 @@ if (process.env.PROD) {
 
 let mainWindow
 
-function createWindow () {
+const createWindow = async () => {
   /**
    * Initial window options
    */
-  mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 1000,
     height: 600,
     useContentSize: true,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
 
-  mainWindow.loadURL(process.env.APP_URL)
+  win.on('ready-to-show', () => {
+    win.show();
+  });
 
-  mainWindow.on('closed', () => {
+  win.on('closed', () => {
     mainWindow = null
-  })
+  });
+
+  await win.loadURL(process.env.APP_URL)
+
+  return win;
+};
+
+// Prevent multiple instances of the app
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
 }
 
-app.on('ready', createWindow)
+app.on('second-instance', () => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore();
+    }
+    mainWindow.show();
+  }
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow()
+    mainWindow = createWindow();
   }
-})
+});
+
+const sendVocabularyToPage = (err, data) => {
+  console.log(data);
+  const vocabularyArray = data.split(/\r?\n/);
+  var filteredArray = vocabularyArray.filter(function (el) {
+    return el != null && el != '';
+  });
+  mainWindow.webContents.send('vocabularyFileLoaded', filteredArray);
+};
+
+const readVocabularyFile = () => {
+  const vocabularyFilePath = path.join('.', '/vocabulary.txt');
+  console.log(vocabularyFilePath);
+  fs.readFile(vocabularyFilePath, 'utf8', sendVocabularyToPage);
+};
+
+(async () => {
+  await app.whenReady();
+  mainWindow = await createWindow();
+  readVocabularyFile();
+})();
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="q-app">
-    <router-view />
+    {{ words }}
+    <!-- <router-view /> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: function () {
+    return {
+      words: []
+    }
+  },
+  beforeMount () {
+    console.log('test')
+    this.$q.electron.ipcRenderer.on('vocabularyFileLoaded', (event, wordsFromVocabularyFile) => {
+      this.words = wordsFromVocabularyFile
+      // console.log(words)
+    })
+  },
+  mounted () {
+    console.log('after mount')
+    // this.$nextTick(function () {
+    // console.log(this.words)
+    // })
+  }
 }
 </script>
 
